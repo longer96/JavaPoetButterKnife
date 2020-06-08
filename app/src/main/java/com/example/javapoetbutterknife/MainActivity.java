84e4dda82c3dbc138dd1302516b325f21307d42f
longer <longer>
2020-06-08 15:50:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/javapoetbutterknife/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.javapoetbutterknife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.annotation.ARouter;
+import com.example.annotation.BindView;
+import com.example.annotation.HelloWorld;
+import com.example.library.ButterKnife;
+
+@ARouter(path = "/app/MainActivity")
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tv.setText("APT  +  JavaPoet");
+    }
+
+    @HelloWorld
+    public void hello() {
+
+    }
+}
